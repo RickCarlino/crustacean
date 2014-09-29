36f230cc0d8081d32d5b3df99f5f22422d4a9759
Rick Carlino <rick.carlino@gmail.com>
2014-09-29 08:02:40 -0500

    Start fact#populate_review testing

--- test/models/fact_test.rb
+++ test/models/fact_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class FactTest < ActiveSupport::TestCase
+  def fact
+    topic.facts.first
+  end
+
   test '#populate_reviews' do
-    assert true
+    fact.populate_reviews(User.create)
+    assert_equal Review.count, 15
+    pending 'Verify that this method is actually creating the appropriate reviews and not just duplicating the same one.'
   end
 end
