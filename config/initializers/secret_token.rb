c60bba75b904e820e9a1359f454ea7206a918e3c
Rick Carlino <rick.carlino@gmail.com>
2014-10-01 22:00:13 -0500

    Such cruft

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,6 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Srs::Application.config.secret_key_base = 'c0cffc6e29285f53cc702c362d6dad15e7070697fd986e1a4556789dbfba1c993d27cfdd8d8ce9c2106cef49a170627d3d4da632d3c28bf2b02d75263be02d86'
+Srs::Application.config.secret_key_base = 'c0cffc6e29285f53cc702c362d6dad15e70'\
+  '70697fd986e1a4556789dbfba1c993d27cfdd8d8ce9c2106cef49a170627d3d4da632d3c28b'\
+  'f2b02d75263be02d86'
