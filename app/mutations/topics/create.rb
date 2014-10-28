94773c52ce494fe9c841904eddac03ca495100f2
Rick Carlino <rick.carlino@gmail.com>
2014-10-28 17:34:19 -0500

    AAnnnnnddddd the train ride is over.

--- app/mutations/topics/create.rb
+++ app/mutations/topics/create.rb
@@ -10,24 +10,28 @@ module Topics
     end
 
     def validate
-      @topic = Topic.new
     end
 
     def execute
       # TODO wrap this in a transaction. Seriously.
-      hmm = question_map
-      binding.pry
+      @topic = Topic.create
+      question_map.map do |ques, counter|
+        binding.pry
+        ques.counter_questions = counter
+      end
       @topic
     end
 
 private
 
+  # Takes a Hash<String:String[]> and turns it into a Hash<Question:Question[]>
+  # Used as an intermediate step for linking questions to a group of counter questions.
   def question_map
     # TODO get fancy with inject or sth.
     question_map = {}
     questions.map do |key, value|
       list = make_questions(value)
-      question_map[Question.find_or_create_by(topic: topic, name: key)] = list
+      question_map[Question.find_or_create_by(topic: topic, name: name)] = list
     end
     question_map
   end
